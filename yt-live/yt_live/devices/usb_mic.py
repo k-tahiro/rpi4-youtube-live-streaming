c50151d142677805a231b686622e3e96644b9033
Keisuke Hirota <tahiro.k.ad@gmail.com>
2023-01-19 00:16:40 +0900

    Support from name

--- yt-live/yt_live/devices/usb_mic.py
+++ yt-live/yt_live/devices/usb_mic.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from .base import Device
 
 
@@ -6,3 +8,18 @@ class UsbMic(Device):
 
     def __init__(self, audio_url: str) -> None:
         super().__init__(audio_fmt=self.AUDIO_FMT, audio_url=audio_url)
+
+    @classmethod
+    def from_name(cls, name: str) -> "UsbMic":
+        cp = subprocess.run(
+            ["pactl", "list", "short", "sources"], capture_output=True, text=True
+        )
+        lines = cp.stdout.splitlines()
+        for i, line in enumerate(lines):
+            if name in line:
+                audio_url = line.strip().split()[1].strip()
+                break
+        else:
+            raise RuntimeError(f'Unable to find "{name}" device')
+
+        return cls(audio_url)
