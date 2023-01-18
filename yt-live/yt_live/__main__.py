c8e56ffe7f071a2ab8ddc822fbbf25f16b0820c2
Keisuke Hirota <tahiro.k.ad@gmail.com>
2023-01-19 00:20:37 +0900

    Refactor

--- yt-live/yt_live/__main__.py
+++ yt-live/yt_live/__main__.py
@@ -1,5 +1,4 @@
 from logging import basicConfig
-from typing import Optional
 
 import typer
 
@@ -10,16 +9,15 @@ from .youtube_live import YouTubeLive
 
 def _main(
     stream_key: str,  # TODO: Get stream key and start live automatically
-    video_url: str,
+    cb_name: str = "MiraBox Capture",
     input_size: str = "480p",
     output_size: str = "480p",
-    audio_url: Optional[str] = None,
     log_level: str = "INFO",
 ) -> None:
     basicConfig(level=log_level)
 
-    capture_board = CaptureBoard(video_url, input_size)
-    usb_mic = UsbMic(audio_url) if audio_url is not None else None
+    capture_board = CaptureBoard.from_name(cb_name, input_size)
+    usb_mic = UsbMic.from_first_device()
 
     youtube_live = YouTubeLive(stream_key, output_size)
     youtube_live.run(capture_board, usb_mic)
